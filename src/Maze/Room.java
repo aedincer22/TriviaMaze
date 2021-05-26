7b18c1f695ec0ad5d1d380fcef942091d8515257
Shirwa Ahmed <sahmed33@uw.edu>
2021-05-26 09:54:59 -0700

    -a

--- src/Maze/Room.java
+++ src/Maze/Room.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 public class Room {
 
-	final private boolean myDoors [];
-	private boolean isLocked;
+	final public boolean myDoors [];
+	//private boolean isLocked; not using islocked
 	
+	/**
+	  *This method initializes room, and creates 
+	  *doors corresponding to the doorCount and sets
+	  *the isLocked value to false
+	  * @param theDoorCount is an integer value that used to setup rooms
+	  */
 	public Room(final int theDoorCount) {
 		
 		if (theDoorCount > 2 || theDoorCount < 0) {
@@ -14,9 +20,13 @@ public class Room {
 		}
 		myDoors = new boolean [theDoorCount];
 		Arrays.fill(myDoors, true);
-		isLocked = false;
+	//	isLocked = false;is locked 
 	}
-	
+	/**
+	  *This method sets the door value 
+	  *from true(open) to false(locked)
+	  *and updates the location of the current door
+	  */	
 	public void lockDoor() {
 		//Shut down one door in the room
 		int index = 0;
@@ -28,6 +38,13 @@ public class Room {
 			index++;
 		}
 	}
+	
+	/**
+	  *This method check to see if 
+	  *any doors are still open if not
+	  *returns a boolean value and used to end game.
+	  * @return boolean true if there is still door opens or false if no doors are open
+	  */
 	public boolean isLocked() {
 		//if all doors are false (meaning they are not open) then room is locked
 		for (boolean door : myDoors) {
@@ -38,6 +55,11 @@ public class Room {
 		return true;
 	}
 	
+	/**
+	  *This method creates a string for the array
+	  *of rooms to be display
+	  * @return String of array display of rooms
+	  */
 	public String toString() {
 		return Arrays.toString(myDoors);
 	}
---
+++ src/Maze/Room_Test.java
@@ -0,0 +1,72 @@
+package Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import Maze.Room;
+
+class Room_Test {
+	
+	//create setup method
+	Room room;
+
+	@Test
+	public void test_Room() {
+		int doorCount = 2;
+		Assertions.assertEquals(2, doorCount);
+	}
+	
+	@Test
+	public void test_RoomDoorCountZero(){
+		int doorCount = 0;
+		Assertions.assertNotEquals(2, doorCount);
+	}
+	
+//	@Test(expected = IllegalArgumentException.class)
+//	public void testIllegalArgumentException() {
+//	    int doorCount = -1;
+//	    ;
+//	}
+	
+	
+	@Test
+	public void testLockDoor() {
+		boolean [] expectedArray = new boolean[] {false,true};
+		room = new Room(2);
+		room.lockDoor();
+		assertArrayEquals(expectedArray, room.myDoors);
+	}
+	
+	@Test
+	public void testIsLockAllDoorsOpen() {
+		room = new Room(2);
+		room.isLocked();
+		Assertions.assertFalse(room.isLocked());
+	}
+	
+	@Test
+	public void testIsLockOneDoorIsOpen() {
+		room = new Room(2);
+		room.lockDoor();
+		room.isLocked();
+		Assertions.assertFalse(room.isLocked());
+	}
+	@Test
+	public void testIsLockNoOPenDoor() {
+		room = new Room(2);
+		room.lockDoor();
+		room.lockDoor();
+		room.isLocked();
+		Assertions.assertTrue(room.isLocked());
+	}
+	@Test
+	public void testToStringEmptyArray() {
+		Room nwRoom = new Room(0);
+		assertEquals("[]", nwRoom.toString());
+	}
+
+	
+}
