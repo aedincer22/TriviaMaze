84be3a48ed0e3b2c2a03de097211c4454cb06d8a
Shirwa331 <83422683+Shirwa331@users.noreply.github.com>
2021-05-25 22:16:17 -0700

    Update Maze_Test.java
    added more test

--- Maze_Test.java
+++ Maze_Test.java
@@ -17,24 +17,42 @@ class Maze_Test {
 	public void testMaze() {
 		int rows = 4;
 		int coloumn = 4;
-		Assertions.assertEquals(4, rows);
-		Assertions.assertEquals(4, coloumn);
 		Assertions.assertEquals(rows, coloumn);
 	}
-
+	
 	@Test
-	public void testBuildMaze() {
-		int rows = 4;
+	public void testMazeColoumn() {
 		int coloumn = 4;
-		maze = new Maze(rows,coloumn);
+		Assertions.assertEquals(4, coloumn);
+	}
+	
+	@Test
+	public void testMazeRows() {
+		int rows = 4;
+		Assertions.assertEquals(4, rows);
+	}
+
+//	@Test
+//	public void testBuildMaze() {
+//		int rows = 4;
+//		int cols = 4;	
+//		Assertions.assertEquals(maze.myRooms[3][3], maze.buildMaze(rows, cols));
+//	}
+	
+	@Test
+	public void testIsNotLastRoom() {
+		maze = new Maze(4,4);
+		maze.move('s');
+		maze.move('e');
+		Assertions.assertEquals(false , maze.isLastRoom());
 	}
 	
 	@Test
 	public void testIsLastRoom() {
-		int rows = 4;
-		int coloumn = 4;
-		maze = new Maze(rows,coloumn);
-		Assertions.assertEquals(false , maze.isLastRoom());
+		maze = new Maze(1,1);
+		maze.move('s');
+		maze.move('e');
+		Assertions.assertEquals(true , maze.isLastRoom());
 	}
 	
 	@Test
@@ -42,11 +60,27 @@ class Maze_Test {
 		Maze  nwMaze = new Maze(1,1);
 		assertEquals("[[[]]]", nwMaze.toString());
 	}
-	
+		
 	@Test
-	public void testMove() {
+	public void testMoveSouth() {
 		char theDirection = 's';
 		 maze.move(theDirection);
+		 Assertions.assertEquals(1, maze.getSouthIndex());
+	}
+	
+	@Test
+	public void testMoveEast() {
+		char theDirection = 'e';
+		 maze.move(theDirection);
+		 Assertions.assertEquals(1, maze.getEastIndex());
+	}
+	
+	@Test
+	public void testMoveWrongInput() {
+		char theDirection = 'x';
+		 maze.move(theDirection);
+		 Assertions.assertEquals(0, maze.getEastIndex());
+		 Assertions.assertEquals(0, maze.getSouthIndex());
 	}
 	
 	@Test
@@ -65,15 +99,38 @@ class Maze_Test {
 	}
 	
 	@Test
+	public void testGetSouthIndexWrong() {
+		Assertions.assertNotEquals(2, maze.getSouthIndex());
+	}
+	
+	@Test
 	public void testGetEastIndex() {
 		assertEquals(0, maze.getEastIndex());
 	}
-//	@Test
-//	public void testGetCurrentRoom() {
-//		int rows = 2;
-//		int coloumn = 2;
-//		maze = new Maze(rows,coloumn);
-//		myRoom = new Room[true];
-//		Assertions.assertEquals(myRoom , maze.getCurrentRoom());
-//	}
+	
+	@Test
+	public void testGetEastIndexWrong() {
+		Assertions.assertNotEquals(2, maze.getEastIndex());
+	}
+		
+	@Test
+	public void testGetCurrentRoomMovedSouth() {
+		maze = new Maze(2,2);
+		maze.move('s');
+		Assertions.assertEquals(maze.myRooms[1][0],maze.getCurrentRoom());
+	}
+	
+	@Test
+	public void testGetCurrentRoomMovedEast() {
+		maze = new Maze(2,2);
+		maze.move('e');
+		Assertions.assertEquals(maze.myRooms[0][1],maze.getCurrentRoom());
+	}
+	
+	@Test
+	public void testGetCurrentRoomMovedWrongRoom() {
+		maze = new Maze(2,2);
+		maze.move('s');
+		Assertions.assertNotEquals(maze.myRooms[1][1],maze.getCurrentRoom());
+	}
 }
